18e03ab5c5d1adafd4806c9d66e4431b66575814
ViPhong <viphongly2804@gmail.com>
2025-07-04 00:36:37 +0700

    feat: enable CORS in global

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,14 @@ import { log } from 'console';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe({
     forbidNonWhitelisted: true,
     transform: true,
   }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
