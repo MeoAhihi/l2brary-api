8d297cf6a900096db0e6d334c3dbd1a3dd53d7f4
ViPhong <viphongly2804@gmail.com>
2025-07-04 01:09:33 +0700

    feat(logger): add request logging middleware and tests

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { log } from 'console';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import {
+  logger
+} from './common/middleware/logger/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
-  app.useGlobalPipes(new ValidationPipe({
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.use(logger);
 
   await app.listen(process.env.PORT ?? 3000);
 }
