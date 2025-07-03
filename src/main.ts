ec49e08d91c63fdd76146fb05eb040c0db80dbd2
ViPhong <viphongly2804@gmail.com>
2025-07-04 01:18:46 +0700

    feat(swagger): add swagger documentation setup for API

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import {
-  logger
-} from './common/middleware/logger/logger.middleware';
+import { logger } from './common/middleware/logger/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +17,15 @@ async function bootstrap() {
   );
 
   app.use(logger);
+
+  const config = new DocumentBuilder()
+    .setTitle('L2BRARY API')
+    .setDescription('HTTP API')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs/api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
